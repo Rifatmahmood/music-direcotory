d810cbbf54024eaf51b126b7a913e084d64517aa
Rifat Mahmood <rftmahmood@gmail.com>
2024-07-09 11:14:24 +0600

    signup-user-login-logout-function based view

--- album/urls.py
+++ album/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import create_album, edit_album
+# from .views import create_album, edit_album
+from .views import CreateAlbumView, EditAlbumView
+# urlpatterns = [
+#     path('add/', create_album, name = "create_album" ), 
+#     path('edit/<int:id>', edit_album , name = "edit_album" ), 
+# ]
 urlpatterns = [
-    path('add/', create_album, name = "create_album" ), 
-    path('edit/<int:id>', edit_album , name = "edit_album" ), 
+    path('create_album/', CreateAlbumView.as_view(), name='create_album'),
+    path('edit_album/<int:id>/', EditAlbumView.as_view(), name='edit_album'),
 ]
--- album/views.py
+++ album/views.py
@@ -1,28 +1,64 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AlbumForm 
 from .models import Album
+from django.views import View
 # Create your views here.
-def create_album(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
+
+
+class CreateAlbumView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            form = AlbumForm()
+            return render(request, 'album_form.html', {'form': form})
+        else:
+            return redirect('login_page')
+
+    def post(self, request):
+        if request.user.is_authenticated:
             form = AlbumForm(request.POST)
             if form.is_valid():
                 form.save()
                 return redirect('musician_list')
+            return render(request, 'album_form.html', {'form': form})
         else:
-            form = AlbumForm()
-            return render(request, 'album_form.html', {"form": form} )
-    else:
-        return redirect('login_page')
+            return redirect('login_page')
 
-        
-def edit_album(request, id):
-    album = get_object_or_404(Album, id=id)
-    if request.method == 'POST':
+class EditAlbumView(View):
+    def get(self, request, id):
+        album = get_object_or_404(Album, id=id)
+        form = AlbumForm(instance=album)
+        return render(request, 'album_form.html', {'form': form})
+
+    def post(self, request, id):
+        album = get_object_or_404(Album, id=id)
         form = AlbumForm(request.POST, instance=album)
         if form.is_valid():
             form.save()
-            redirect('musician_list')
-    else:
-        form = AlbumForm(instance=album)
-    return render(request, 'album_form.html', {'form': form})
+            return redirect('musician_list')
+        return render(request, 'album_form.html', {'form': form})
+
+
+# def create_album(request):
+#     if request.user.is_authenticated:
+#         if request.method == "POST":
+#             form = AlbumForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect('musician_list')
+#         else:
+#             form = AlbumForm()
+#             return render(request, 'album_form.html', {"form": form} )
+#     else:
+#         return redirect('login_page')
+
+        
+# def edit_album(request, id):
+#     album = get_object_or_404(Album, id=id)
+#     if request.method == 'POST':
+#         form = AlbumForm(request.POST, instance=album)
+#         if form.is_valid():
+#             form.save()
+#             redirect('musician_list')
+#     else:
+#         form = AlbumForm(instance=album)
+#     return render(request, 'album_form.html', {'form': form})
